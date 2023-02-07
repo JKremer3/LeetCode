f983d3d52be92280d7921a3849a90ea2ee75f655
JKremer3 <jeffrey.kremer.iii@gmail.com>
2023-02-07 14:42:39 -0800

    Add base for medium difficulty Math LC with first problem

---
+++ MathLC/ModerateMathLC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ModerateMathLC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test for Max Area of a rectangle
+            /*var testArray = new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7};
+            Console.WriteLine(MaxArea(testArray));*/
+        }
+
+        static int MaxArea(int[] height)
+        {
+            int i = 0, j = height.Length-1, max = 0;
+
+            while (i < j)
+            {
+                max = Math.Max(max, (j - i) * Math.Min(height[i], height[j]));
+
+                if (height[i] > height[j])
+                {
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return max;
+        }
+    }
+}

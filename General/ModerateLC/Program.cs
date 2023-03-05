c6af47d3374fc9bf818bd569e4249e6cdbd8e4b8
JKremer3 <jeffrey.kremer.iii@gmail.com>
2023-03-05 15:02:29 -0800

    Add Project and files for Moderate LC problems that aren't math

---
+++ General/ModerateLC/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ModerateLC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var intList = new int[]{1,2,3,4};
+            runRemoveNthNode(intList, 2);
+        }
+
+        static void runRemoveNthNode(int[] nodeList, int n)
+        {
+            var head = new ListNode(nodeList[0]);
+            var curr = head;
+
+            for (int i = 1; i < nodeList.Length; i++)
+            {
+                var newNode = new ListNode(nodeList[i]);
+                curr.next = newNode;
+                curr = curr.next;
+            }
+
+            var modifiedList = removeNthNodeFromList.RemoveNthFromEnd(head, n);
+
+            if (modifiedList == null)
+            {
+                Console.WriteLine("[]");
+            }
+            else
+            {
+                var outString = "[";
+                while (modifiedList != null)
+                {
+                    outString += modifiedList.val.ToString() + ", ";
+                    modifiedList = modifiedList.next;
+                }
+
+                outString += "]";
+                Console.WriteLine(outString);
+            }
+        }
+    }
+}
---
+++ General/ModerateLC/removeNthNodeFromList.cs
@@ -0,0 +1,41 @@
+﻿namespace ModerateLC
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+    public static class removeNthNodeFromList
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var startNode = new ListNode(0);
+            var laggingNode = startNode;
+            var leadingNode = startNode;
+            
+            laggingNode.next = head;
+
+            //Move leading node N spaces in front of the lagging node
+            for (int i = 0; i <= n; i++)
+                leadingNode = leadingNode.next;
+
+            //Progress both leading and lagging node through the list, until the lead node reaches the end of the list
+            while (leadingNode != null)
+            {
+                laggingNode = laggingNode.next;
+                leadingNode = leadingNode.next;
+            }
+
+            //skip the node to be removed, then return
+            laggingNode.next = laggingNode.next.next;
+            return startNode.next;
+        }
+    }
+}
